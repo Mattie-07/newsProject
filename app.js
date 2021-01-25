4e926d0e2e46c03e4a759c6e7ec5dd83659b0e4c
Matthew <Matthewtroberts10@gmail.com>
2021-01-25 15:48:27 -0600

    framework to the frontpage added, some css styling

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ app.set('view engine', 'ejs');
 //public folder
 app.use(express.static('public'));
 
+//routes
+app.use(require('./routes/index'));
+app.use(require('./routes/registration'));
 
-
-
+//access to the public folder
+app.use(express.static('public'));
 
 
 
--- routes/registration.js
+++ routes/registration.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 
+router.get('/registration', ( req, res) => {
+    res.render('registration')
+})
+
+
 
 
 
