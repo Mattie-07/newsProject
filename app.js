7cab1f46e01506bd411351ffd5727126e8b5cc5f
Kanny <kanny.ghafour87@gmail.com>
2021-01-26 15:48:05 -0600

    install helmet

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
 
 
 app.use(express.urlencoded({extended: false}));
@@ -11,6 +12,7 @@ app.set('view engine', 'ejs');
 
 //public folder
 app.use(express.static('public'));
+app.use(helmet());
 
 //routes
 app.use(require('./routes/index'));
