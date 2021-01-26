c655a0539a59a95d11a82729eba1738f4c673b7b
Matthew Chun <matthewchun93@gmail.com>
2021-01-26 07:51:33 -0800

    merge main

---
+++ setup.bash
@@ -0,0 +1,4 @@
+
+sequelize model:generate --name admin --attributes username:string,password:string,email:string
+
+sequelize model:generate --name users --attributes username:string,password:string,password:string,roleID:integer
