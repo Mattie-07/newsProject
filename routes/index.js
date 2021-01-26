632ad186adaa8e8fb24fea8f1d69a9285f9e189c
Kim Long <kimrlong09@gmail.com>
2021-01-26 11:25:56 -0500

    merge

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 
+
+router.get('/' , (req, res) =>{
+        res.render('index')
+})
+
 //const bodyParser = require('body-parser');
 //router.use(express.urlencoded({extended: false}));
 // router.use(express.json())
-
-router.get('/index', (req, res) => {
-  
-    res.send('index')
-})
 
 module.exports = router;
--- routes/registration.js
+++ routes/registration.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 
+
 //const bodyParser = require('body-parser');
 //router.use(express.urlencoded({extended: false}));
 // router.use(express.json())
 
+
 router.get('/:registration', (req, res) => {
-  
+
     res.send('registration')
 })
 module.exports = router;
