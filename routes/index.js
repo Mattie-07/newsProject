fba1af0db734ba21b428a0064433136ee3142273
Kanny <kanny.ghafour87@gmail.com>
2021-02-03 09:47:27 -0600

    readMe

--- routes/index.js
+++ routes/index.js
@@ -20,50 +20,50 @@ router.get('/error', (req, res)=>{
 })
 
 
-// ------------------search API --------------------------------------------------
-const searchEngine = document.querySelector('.search');
-const input = document.querySelector('.input');
-const newsReturn = document.querySelector('.news-return');
+// // ------------------search API --------------------------------------------------
+// const searchEngine = document.querySelector('.search');
+// const input = document.querySelector('.input');
+// const newsReturn = document.querySelector('.news-return');
 
-searchEngine.addEventListener('submit', retrieve);
+// searchEngine.addEventListener('submit', retrieve);
 
-function retrieve(e){
+// function retrieve(e){
 
-    if(input.value == ''){
-        alert('Input field is empty!')
-        return
-    }
+//     if(input.value == ''){
+//         alert('Input field is empty!')
+//         return
+//     }
 
-    newsReturn.innerHTML = '';
+//     newsReturn.innerHTML = '';
 
-    e.preventDefault()
+//     e.preventDefault()
 
-    const apiKey = `7235e9c528744dd09cfafa0d2cc35723`
-    let topic = input.value;
+//     const apiKey = `7235e9c528744dd09cfafa0d2cc35723`
+//     let topic = input.value;
 
-    let url = `http://newsapi.org/v2/everything?q=${topic}&apiKey=${apiKey}`
+//     let url = `http://newsapi.org/v2/everything?q=${topic}&apiKey=${apiKey}`
 
-    fetch(url).then((res)=>{
-        return res.json()
-               //return response in JSON format
-    })
-    .then((data)=>{
-        console.log(data)
-        data.articles.forEach(article =>{
-            let li = document.createElement('li');
-            let a = document.createElement('a');
-            a.setAttribute('href', data)
-            a.setAttribute('target', '_blank');
-            a.textContent = article.url;  //what would i do instead of textContent??
-            li.appendChild(a);
-            newsReturn.appendChild(li);
-        })
-    }).catch((error)=>{
-        console.log(error)
-    });
-}
+//     fetch(url).then((res)=>{
+//         return res.json()
+//                //return response in JSON format
+//     })
+//     .then((data)=>{
+//         console.log(data)
+//         data.articles.forEach(article =>{
+//             let li = document.createElement('li');
+//             let a = document.createElement('a');
+//             a.setAttribute('href', data)
+//             a.setAttribute('target', '_blank');
+//             a.textContent = article.url;  //what would i do instead of textContent??
+//             li.appendChild(a);
+//             newsReturn.appendChild(li);
+//         })
+//     }).catch((error)=>{
+//         console.log(error)
+//     });
+// }
 
-console.log(topic);
+// console.log(topic);
 
 // router.get('/logout', (req, res) => {
 //   //session is cleared
